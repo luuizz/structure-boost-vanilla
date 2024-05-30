76d2186a270f6909b50e198f937c56c12622fbcb
luuizz <luu.izz@hotmail.com>
2024-05-30 16:30:11 -0300

    feat: header responsivo e fixo

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,23 +1,60 @@
+"use client"
 import Link from "next/link";
 import Container from "../Container/container";
 import Image from "next/image";
 import Logo from "@/assets/logo.svg"
 import BtnMain from "../Btn/button";
-import { HeaderStyle, HeaderContainer, NavHeader } from "./style.css";
+import { HeaderStyle, HeaderContainer, NavHeader, asideMenuStyle } from "./style.css";
 import Navigation from "./Navigation";
+import Hamburger from "hamburger-react";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+
+    const [isOpen, setOpen] = useState(false);
+    const [isFixed, setFixed] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if(window.scrollY > 50) {
+                setFixed(true)
+            } else {
+                setFixed(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, []);
+
+    const closeMenu = () => {
+        setOpen(false);
+    };
+
     return (
-        <header className={HeaderStyle}>
-            <Container className={HeaderContainer}>
+        <header className={`${HeaderStyle} ${isFixed ? 'fixed' : ''}`}>
+            <Container className={`${HeaderContainer} container`}>
                 <Link href="/" title="Página inicial">
                     <Image src={Logo} alt="Logo Structureboost" />
                 </Link>
                     <nav className={NavHeader}>
                         <Navigation />
-                        <BtnMain variant="default" label="Entre em contato" url="#" title="Entre em contato" />              
+                        <BtnMain variant="default" label="Entre em contato" url="/contato" title="Entre em contato" />
                     </nav>
+                    <Hamburger label="Mostrar menu" toggled={isOpen} toggle={setOpen} />    
             </Container>
+
+            <aside className={`${asideMenuStyle} ${isOpen ? 'open' : ''} ${isFixed ? 'fixed' : ''}`}>
+                <div className="container">
+                    <nav>
+                        <Navigation closeMenu={closeMenu} />
+                        <BtnMain variant="default" label="Entre em contato" url="#" title="Entre em contato" />
+                    </nav>
+                </div>
+        </aside>
         </header>
     )
 }
--- src/components/Header/style.css.js
+++ src/components/Header/style.css.js
@@ -2,23 +2,48 @@ import { style } from "@vanilla-extract/css";
 
 export const HeaderStyle = style({
     width: "100%",
-    height: 88,
-    padding: "2rem 0",
     display: "flex",
+    padding: "2rem 0px",
     alignItems: "center",
-    borderBottom: "1px solid rgba(255,255,255,.24)",
+    transition: "all 0.3s ease 0s",
+    position: "fixed",
+    zIndex: 2023,
+    backgroundColor: "var(--gray-900)",
+    "&.fixed": {
+        padding: "1.4rem 0px",
+        borderBottom: "1px solid rgba(255, 255, 255, 0.24)",
+    },
+    ".container": {
+        ".hamburger-react": {
+            display: "none"
+        },
+    },
+    '@media': {
+        "(max-width: 991px)": {
+            ".container": {
+                ".hamburger-react": {
+                    display: "block",
+                }
+            }
+        }
+    }
 })
 
 export const HeaderContainer = style({
     display: "flex",
     alignItems: "center",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
 })
 
 export const NavHeader = style({
     display: "flex",
     alignItems: "center",
-    gap: 32
+    gap: 32,
+    '@media': {
+        '(max-width: 991px)': {
+            display: 'none'
+        }
+    }
 })
 
 export const LinksHeader  = style({
@@ -36,5 +61,37 @@ export const LinkStyle = style({
     fontFamily: "var(--font-satoshi)",
     ":hover": {
         opacity: .5,
+    }
+})
+
+export const asideMenuStyle = style({
+    padding: "6.4rem 0px",
+    width: "100%",
+    height: "calc(100vh - var(--header-height))",
+    backgroundColor: 'var(--black)',
+    opacity: 0,
+    pointerEvents: "none",
+    top: "var(--header-height)",
+    transition: "all 0.3s ease 0s",
+    transform: "translateY(100%)",
+    zIndex: 50,
+    position: "fixed",
+    "&.open": {
+        pointerEvents: "all",
+        opacity: 1,
+        transform: "translateY(0px)",
+    },
+    ".container": {
+        "nav": {
+            "ul": {
+                flexDirection: "column",
+                marginBottom: "4rem",
+                "li": {
+                    "a": {
+                        fontSize: "3rem",
+                    }
+                }
+            }
+        }
     }
 })
