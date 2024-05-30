23dde4f21088fa79401c7f3f5b3162219a261350
luuizz <luu.izz@hotmail.com>
2024-05-30 16:32:57 -0300

    fix: inserido componente genérico

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,17 +1,17 @@
 import BtnMain from '@/components/Btn/button'
 import Container from '@/components/Container/container'
+import Generic from '@/components/Generic/generic'
 import React from 'react'
 
 export default function NotFound() {
   return (
     <>
-        <div className="generic">
+        <Generic>
             <Container>
-
                 <h1 style={{marginBottom: '4rem'}}>Página não encontrada</h1>
                 <BtnMain title="Voltar para página inicial" label="Voltar para página inicial" url="/" variant="white lg" />
             </Container>
-        </div>
+        </Generic>
     </>
   )
 }
